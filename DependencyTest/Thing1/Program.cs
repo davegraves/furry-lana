677b41532f1f064eb84907aead0bff40ce278137
Dave Graves <dgraves@incomm.com>
2014-07-02 13:18:44 -0700

    yeah

---
+++ DependencyTest/Thing1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thing1
+{
+    using Core1;
+    using Core2;
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            new Class1();
+            new Class2();
+        }
+    }
+}
